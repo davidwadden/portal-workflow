dbd48df30ba0bbcfa80620ecedeee3768fc516bd
David Wadden <dwadden@pivotal.io>
2018-09-17 18:13:05 -0500

    Bootstraps from Spring Initializr

---
+++ src/test/java/io/pivotal/cnde/portal/portalworkflow/PortalWorkflowApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.cnde.portal.portalworkflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortalWorkflowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
