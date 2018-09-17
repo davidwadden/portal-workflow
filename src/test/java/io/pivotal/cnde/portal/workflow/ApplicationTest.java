3f5d6d0cc04ef6c2ebec7fda581ddd42fb215da3
David Wadden <dwadden@pivotal.io>
2018-09-17 18:35:11 -0500

    Lints project

--- src/test/java/io/pivotal/cnde/portal/workflow/ApplicationTest.java
+++ src/test/java/io/pivotal/cnde/portal/workflow/ApplicationTest.java
@@ -1,4 +1,4 @@
-package io.pivotal.cnde.portal.portalworkflow;
+package io.pivotal.cnde.portal.workflow;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PortalWorkflowApplicationTests {
+public class ApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
